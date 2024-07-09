315c0ed1b9e32fa36b44fb4469712bb5625254d8
Boppudi Revanth <132438481+Revanth192210076@users.noreply.github.com>
2024-07-09 13:29:16 +0530

    Add files via upload

---
+++ insert element in list.py
@@ -0,0 +1,11 @@
+l = [9, 3, 6, 2, 7, 1]
+
+print("Original array: ", l)
+
+specified_index = 4
+element = 10
+
+
+l.insert(specified_index, element)
+
+print("Array after inserting the element: ", l)
---
+++ read file.py
@@ -0,0 +1,5 @@
+with open('brhv.txt','r') as file:
+	
+	for line in file:
+		for word in line.split():		 
+			print(word)
