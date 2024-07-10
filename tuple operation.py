255c6c30efd1b2dfcb2133d62cf3a32227b09773
Boppudi Revanth <132438481+Revanth192210076@users.noreply.github.com>
2024-07-10 08:58:30 +0530

    Add files via upload

---
+++ tuple operation.py
@@ -0,0 +1,23 @@
+t = (1, 4, 2, 7, 3, 9)
+print (max(t))
+print (min(t))
+
+
+t = (2, 5, 0) + (1, 3) + (4,)
+print (t);
+
+
+t = (1, 2, 3, 4)
+print(t[2:4])
+
+
+
+test_tup = (1, 3)
+
+print("The original tuple : " + str(test_tup))
+
+N = 4
+
+res = ((test_tup, ) * N)
+print("The duplicated tuple elements are : " + str(res))
+
